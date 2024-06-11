31a29d99c25770c4621f19d5a10b828555ebb61e
MagnetKim <namchic77@yonsei.ac.kr>
2024-06-11 22:01:14 +0900

    Update ml.py

--- ml.py
+++ ml.py
@@ -72,11 +72,9 @@ def run_ml_app():
             else:
                 return '그 이하'
         
-        st.subheader("학교 정보")
-        
         # Academic Achievement
-        st.write("학업성취도 수준을 선택하세요.")
-        matach_score = st.slider('학업성취도 수준', 0, 1000, 500)
+        st.write("학업성취도 수준을 선택하세요. 625점 이상: ‘수월’, 550점: ‘우수’, 475점: ‘보통’, 400점: ‘기초’")
+        matach = st.slider('학업성취도 수준', 0, 1000, 500, step=100)
         
         # Non-Korean Speaking Students Ratio
         st.write("한국어를 안 쓰는 학생 비율을 선택하세요.")
@@ -115,7 +113,7 @@ def run_ml_app():
         stchrcap = st.slider('교사능력에 대한 학생들의 인식', 1, 4, 1)
         
         input_data_others = {
-            'MATACH': categorize_matach(matach_score),
+            'MATACH': matach_score,
             'SCHKOR': schkor,
             'SCHPOP': schpop,
             'SCHPOOR': schpoor,
